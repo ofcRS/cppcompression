807a17fa49dd37ced3e531c427bba9a3254ec971
ofcRS <azeric054@gmail.com>
2022-05-07 23:27:21 +0400

    common lz77 compression

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include "string"
 #include "vector"
 
-const std::string str = "sir sid eastman Nulla lobortis nunc at cursus malesuada. Integer quis tortor diam. In et congue tortor. Sed a est sed nisi tincidunt vulputate a eu urna. Nulla felis lorem, euismod vel erat id, scelerisque faucibus velit. Curabitur hendrerit leo eu nunc ornare bibendum. Nulla vulputate mollis ligula, ornare venenatis libero aliquet sed. Suspendisse et ultrices lectus. Vestibulum facilisis mattis sem a aliquet. Etiam at sodales est. Sed non consequat est. Nam vestibulum, risus ut rhoncus eleifend, ligula nisi malesuada ipsum, quis mattis odio sem sit amet lorem. Sed tempor accumsan justo, ut rutrum felis consectetur non. Etiam ultricies mi mauris, sed pretium ipsum tempus sed. Aenean commodo facilisis sem.";
+//const std::string str = "sir sid eastman Nulla lobortis nunc at cursus malesuada. Integer quis tortor diam. In et congue tortor. Sed a est sed nisi tincidunt vulputate a eu urna. Nulla felis lorem, euismod vel erat id, scelerisque faucibus velit. Curabitur hendrerit leo eu nunc ornare bibendum. Nulla vulputate mollis ligula, ornare venenatis libero aliquet sed. Suspendisse et ultrices lectus. Vestibulum facilisis mattis sem a aliquet. Etiam at sodales est. Sed non consequat est. Nam vestibulum, risus ut rhoncus eleifend, ligula nisi malesuada ipsum, quis mattis odio sem sit amet lorem. Sed tempor accumsan justo, ut rutrum felis consectetur non. Etiam ultricies mi mauris, sed pretium ipsum tempus sed. Aenean commodo facilisis sem.";
+const std::string str = "sir sid eastman";
+
 
 const int BUFFER_LENGTH = 10;
 
@@ -18,22 +20,41 @@ int main() {
     int buffer_start = 0;
 
     while (buffer_start < str.length()) {
-        for (int i = buffer_start; i >= 0; i--) {
-               int match_length = 0;
-               //if (str[buffer_start] == str[i]) {
-               //        int j = 0;
-               //        while (str[buffer_start + j] == str[i + j]) {
-               //                match_length++;
-               //                j++;
-               //        }
-               //}
-               auto token = output_token();
-               token.offset = i;
-               token.length = match_length;
-               token.next_symbol = str[i + match_length];
-	       output.push_back(token);
+        std::cout << "buffer_start = " << buffer_start << std::endl;  
+        int max_match_length = 0;
+	int max_match_offset = 0;
+        for (int i = buffer_start - 1; i >= 0; i--) {
+               int current_length = 0;
+
+               if (str[buffer_start] == str[i]) {
+                       int j = 0;
+                       while (str[buffer_start + j] == str[i + j]) {
+                               current_length++;
+                               j++;
+                       }
+               }
+	       if (current_length > max_match_length) {
+		       max_match_length = current_length;
+		       max_match_offset = buffer_start - i;
+	       }
+               //std::cout << "max match offset = " << max_match_offset << std::endl;
+
         }
-        buffer_start++;
+        if (max_match_length > 0) {
+               auto token = output_token();
+               token.offset = max_match_offset;
+               token.length = max_match_length;
+               token.next_symbol = str[buffer_start + max_match_length];
+	       output.push_back(token);
+	       buffer_start += max_match_length + 1;
+	} else {
+               auto token = output_token();
+               token.offset = 0;
+               token.length = 0;
+               token.next_symbol = str[buffer_start];
+	       output.push_back(token);
+               buffer_start++;
+	}
     }
 
     for (auto &item : output) {
