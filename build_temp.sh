af1b07e35fbc4ef1757ddab3e340474851952ed1
ofcRS <azeric054@gmail.com>
2022-05-22 21:31:16 +0400

    modify huffman code function

---
+++ build_temp.sh
@@ -0,0 +1,1 @@
+g++ -std=c++17 -o temp_out ./src/helpers/huffman_codding.cpp && ./temp_out
--- src/helpers/huffman_codding.cpp
+++ src/helpers/huffman_codding.cpp
@@ -5,7 +5,7 @@
 
 
 struct NodeTree {
-    int value;
+    float value;
     NodeTree *left;
     NodeTree *right;
 };
@@ -13,7 +13,7 @@ struct NodeTree {
 int main() {
     std::string str = "abcdefff";
     
-    std::unordered_map<char, int> chars_counting = {};
+    std::unordered_map<char, float> chars_counting = {};
 
     for (int i = 0; i < str.length(); i++) {
         char c = str[i];
@@ -21,6 +21,33 @@ int main() {
     }
 
     for (const auto &n : chars_counting) {
-        std::cout << n.first << " " << n.second << std::endl;
+        chars_counting[n.first] = chars_counting[n.first] / str.length(); 
+    }
+    
+    std::vector<NodeTree> nodes_list;
+    
+
+    for (const auto &n : chars_counting) {
+        NodeTree node = NodeTree();
+        node.left = nullptr;
+        node.right = nullptr;
+        node.value = n.second;
+        nodes_list.push_back(node);
+    }
+
+    while (nodes_list.length() > 0) {
+        vector<int> min_indecies;
+
+        while (min_indecies.length() < 2) {
+            int min_value = INT_MAX;
+            
+            for (int i = 0; i < nodes_list.length(); i++) {
+                
+            }
+        }
+    }
+
+    for (const auto &item : nodes_list) {
+        std::cout << item.value << std::endl;
     }
 }
